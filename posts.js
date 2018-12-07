ce428d9882d90a3797f82c2585a69d5fcae6b2e7
isaink <inks.minds@gmail.com>
2018-12-07 17:25:36 -0500

    one step more

--- posts.js
+++ posts.js
@@ -9,8 +9,16 @@ router.get("/", (req, res) => { // /post - JSON list of all posts.
   })
 })
 router.get("/:id", (req, res) => { // /post/:id - JSON of a single post with the given ID.
-  let id = req.params.id;
-  res.json({posts}.posts[id -1]);
+  // let id = req.params.id;
+  // res.json({posts}.posts[id -1]);
+  posts.forEach(posts => {
+    if(posts.id === Number(req.params.id)){
+      res.json(
+        {chosenPosts: posts,}
+      )
+    }
+  })
+
 });
 router.get("/user/:id", (req, res) => { // /post/user/:id - JSON of all posts that the user with the given ID wrote (right now, for the data we have, it should just be one post).
   let userId = req.params.userId;
